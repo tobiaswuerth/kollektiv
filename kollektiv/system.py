2b1e78a2b1d6c1bfff2da1e0d83adf2f8ce04a16
tobiaswuerth <21124705+tobiaswuerth@users.noreply.github.com>
2025-04-28 10:12:08 +0200

    hello custom state

--- kollektiv/system.py
+++ kollektiv/system.py
@@ -4,10 +4,11 @@ from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langchain.chat_models import init_chat_model
 from langchain_community.tools import DuckDuckGoSearchRun
-from langchain_core.tools import tool
+from langchain_core.tools import InjectedToolCallId, tool
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.types import Command, interrupt
+from langchain_core.messages import ToolMessage
 
 
 class State(TypedDict):
@@ -15,12 +16,44 @@ class State(TypedDict):
     # in the annotation defines how this state key should be updated
     # (in this case, it appends messages to the list, rather than overwriting them)
     messages: Annotated[list, add_messages]
+    name: str
+    birthday: str
 
 
+# Note that because we are generating a ToolMessage for a state update, we
+# generally require the ID of the corresponding tool call. We can use
+# LangChain's InjectedToolCallId to signal that this argument should not
+# be revealed to the model in the tool's schema.
 @tool(description="Requests human assistance")
-def human_assistance(query: str) -> str:
-    human_response = interrupt({"query": query})
-    return human_response["data"]
+def human_assistance(name: str, birthday: str, tool_call_id: Annotated[str, InjectedToolCallId]) -> str:
+    """Request assistance from a human."""
+    human_response = interrupt(
+        {
+            "question": "Is this correct?",
+            "name": name,
+            "birthday": birthday,
+        },
+    )
+    # If the information is correct, update the state as-is.
+    if human_response.get("correct", "").lower().startswith("y"):
+        verified_name = name
+        verified_birthday = birthday
+        response = "Correct"
+    # Otherwise, receive information from the human reviewer.
+    else:
+        verified_name = human_response.get("name", name)
+        verified_birthday = human_response.get("birthday", birthday)
+        response = f"Made a correction: {human_response}"
+
+    # This time we explicitly update the state with a ToolMessage inside
+    # the tool.
+    state_update = {
+        "name": verified_name,
+        "birthday": verified_birthday,
+        "messages": [ToolMessage(response, tool_call_id=tool_call_id)],
+    }
+    # We return a Command object in the tool to update our state.
+    return Command(update=state_update)
 
 
 def visualize_graph(graph):
@@ -37,9 +70,13 @@ def visualize_graph(graph):
 class System:
     def __init__(self):
         self.search = DuckDuckGoSearchRun()
-        self.tools = [self.search, human_assistance]
+        self.tools = [
+            self.search,
+            human_assistance,
+        ]
 
-        self.llm = init_chat_model("ollama:mistral-nemo")
+        # self.llm = init_chat_model("ollama:mistral-nemo")
+        self.llm = init_chat_model("ollama:mistral-small3.1:latest")
         self.llm_with_tools = self.llm.bind_tools(self.tools)
 
         self.memory = MemorySaver()
@@ -47,14 +84,16 @@ class System:
     def run(self):
 
         def chatbot(state: State):
-            return {"messages": [self.llm_with_tools.invoke(state["messages"])]}
+            message = self.llm_with_tools.invoke(state["messages"])
+            assert len(message.tool_calls) <= 1
+            return {"messages": [message]}
 
         # The argument is the function or object that will be called whenever
         # the node is used.
         graph_builder = StateGraph(State)
 
-        graph_builder.add_edge(START, "chatbot")
         graph_builder.add_node("chatbot", chatbot)
+        graph_builder.add_edge(START, "chatbot")
 
         tool_node = ToolNode(tools=self.tools)
         graph_builder.add_node("tools", tool_node)
@@ -64,7 +103,9 @@ class System:
             tools_condition,
         )
         graph_builder.add_edge("tools", "chatbot")  # retour
-        graph = graph_builder.compile(checkpointer=self.memory)
+        graph = graph_builder.compile(
+            checkpointer=self.memory,
+        )
 
         # visualize_graph(graph)
 
@@ -77,16 +118,28 @@ class System:
                 stream_mode="values",
             )
             for event in events:
-                event["messages"][-1].pretty_print()
+                if "messages" in event:
+                    event["messages"][-1].pretty_print()
+                else:
+                    print(event)
 
-        while True:
-            try:
-                user_input = input("User: ")
-                if user_input.lower() in ["quit", "exit", "q"]:
-                    print("Goodbye!")
-                    break
 
-                stream_graph_updates(user_input)
-            except Exception as e:
-                print(f"Error: {e}")
-                break
+        user_input = (
+            "Lookup when LangGraph was released. "
+            "When you have the answer, pass your best guess to the human_assistance tool for review. "
+            "Do not ask for confirmation and do use the tools required."
+        )
+        stream_graph_updates(user_input)
+
+        # while True:
+        #     try:
+
+        #         user_input = input("User: ")
+        #         if user_input.lower() in ["quit", "exit", "q"]:
+        #             print("Goodbye!")
+        #             break
+
+        #         stream_graph_updates(user_input)
+        #     except Exception as e:
+        #         print(f"Error: {e}")
+        #         break
